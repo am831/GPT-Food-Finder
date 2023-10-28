e7a2a6bca129fcf6df1d8fe4340588b0a5ab3a95
Han K Ngo <100813911+hankngo@users.noreply.github.com>
2023-10-28 15:06:47 -0700

    chat bot

--- app.py
+++ app.py
@@ -76,5 +76,32 @@ def search():
     }
     return _request(YELP_HOST, SEARCH_PATH, yelp_api_key, url_params)
 
+def chatbot():
+  # Create a list to store all the messages for context
+  messages = []
+
+  # Keep repeating the following
+  while True:
+    # Prompt user for input
+    message = input("User: ")
+
+    # Exit program if user inputs "quit"
+    if message.lower() == "quit":
+        break
+
+    # Add each new message to the list
+    messages.append({"role": "user", "content": message})
+
+    # Request gpt-3.5-turbo for chat completion
+    response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=messages
+    )
+
+    # Print the response and add it to the messages list
+    chat_message = response['choices'][0]['message']['content']
+    print(f"Bot: {chat_message}")
+    messages.append({"role": "assistant", "content": chat_message})
+
 if __name__ == '__main__':
     app.run(debug=True)
