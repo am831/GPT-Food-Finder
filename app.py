cc4a3692c6ea0f76d0bbd5419390092d8a253fef
Alisha <alisha831maddy@gmail.com>
2023-10-28 08:52:57 -0700

    search

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 import requests
 from geopy.geocoders import Nominatim
 import os
+from urllib.parse import quote
 
 app = Flask(__name__)
 
@@ -12,21 +13,33 @@ with open(".env") as f:
 
 # Get the API key from the environment variable
 yelp_api_key = os.environ.get("YELP_API")
+YELP_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+YELP_SEARCH_LIMIT = 3
 headers = {'Authorization': 'Bearer {}'.format(yelp_api_key),'accept': 'application/json'}
 
 @app.route('/test')
 def hello_world():
     return jsonify({"message": "Hello, World!"})
 
-@app.route('/location')
-def get_user_location():
+def _get_user_location():
     ip_response = requests.get("https://ipinfo.io")
-    
-    user_ip = ip_response.json()["ip"]
     loc = ip_response.json()["loc"]
     city = ip_response.json()["city"]
+    return loc, city
 
-    return jsonify({"ip": user_ip}, {"Coords": loc}, {"city": city})
+def _request(host, path, api_key, url_params=None):
+    url_params = url_params or {}
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    headers = {
+        'Authorization': 'Bearer %s' % api_key,
+    }
+
+    print(u'Querying {0} ...'.format(url))
+
+    response = requests.request('GET', url, headers=headers, params=url_params)
+
+    return response.json()
   
 
 @app.route('/business/<business_id_or_alias>')
@@ -41,6 +54,16 @@ def get_by_id(business_id_or_alias):
     business_path = f'https://api.yelp.com/v3/businesses/{business_id_or_alias}'
     business_response = requests.get(business_path, headers=headers)
     return business_response.json()
+
+@app.route('/search')
+def search():
+    coords, city = _get_user_location()
+    url_params = {
+    'term': "dinner",
+    'location': coords.replace(' ', '+'),
+    'limit': YELP_SEARCH_LIMIT
+    }
+    return _request(YELP_HOST, SEARCH_PATH, yelp_api_key, url_params)
 
 # @app.route('/business/search/<phone>')
 # def get_business_location(phone):
