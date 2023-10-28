ec26d58820c3a899255859c94a3a31bc1b7168b9
Alisha <alisha831maddy@gmail.com>
2023-10-27 21:20:49 -0700

    get ip

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
+from geopy.geocoders import Nominatim
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/test')
 def hello_world():
-    return 'Hello, World!'
+    return jsonify({"message": "Hello, World!"})
+
+@app.route('/location')
+def get_user_location():
+    ip_response = requests.get("https://ipinfo.io")
+    user_ip = ip_response.json()["ip"]
+    geolocator = Nominatim(user_agent="https://query-csv.streamlit.app/")
+    location = geolocator.geocode(user_ip)
+    return jsonify({"City:": location}, {"ip": user_ip})
 
 if __name__ == '__main__':
     app.run()
