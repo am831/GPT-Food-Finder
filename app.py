475283a0378ee4af7c03a61d530506e71af8776d
Alisha Maddy <alisha831maddy@gmail.com>
2023-10-29 09:24:33 -0700

    switch to fast api

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, jsonify, request, render_template
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import Any, List
 import requests
 import os
-from urllib.parse import quote
 import json
 import openai
-from flask_cors import CORS
-from typing import Any
 import time
 import uuid
 
-app = Flask(__name__)
-CORS(app)
-app.config['index.html'] = True
+app = FastAPI()
+
+# Enable CORS to allow cross-origin requests
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def load_env(file_path='.env'):
     try:
@@ -22,64 +29,46 @@ def load_env(file_path='.env'):
                     os.environ[key] = value
     except FileNotFoundError:
         print(f"{file_path} not found. Make sure the .env file exists.")
+
 load_env()
 yelp_api_key = os.environ.get("YELP_API")
-messages = []
 openai_api_key = os.environ.get("OPENAI_API")
+openai.api_key = openai_api_key
 YELP_HOST = 'https://api.yelp.com'
 SEARCH_PATH = '/v3/businesses/search'
 YELP_SEARCH_LIMIT = 20
-headers = {'Authorization': 'Bearer {}'.format(yelp_api_key),'accept': 'application/json'}
 
-@app.route('/', methods=['POST', 'GET'])
-def init():
-    location = request.get_json()
-    if location is None:
-        return jsonify(error='Invalid JSON'), 400
-    latitude, longitude = location.get('latitude'), location.get('longitude')
-    response = jsonify(success=True)
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    data, extra_info = get_restaurant_info(latitude, longitude)
-    data_string = json.dumps(data)
-    messages.append({"role": "user", "content": "Here is data about restaurants in JSON format. Use this data to answer my questions. " + data_string})
-    messages.append({"role": "user", "content": "Here is extra info about restaurants in JSON format. Use this data to answer my questions. " + extra_info})
-    return render_template('index.html')
+class Location(BaseModel):
+    latitude: float
+    longitude: float
 
-"""
-@app.route('/location', methods=['POST', 'GET'])
-def _get_user_location():
-    location = request.get_json()
-    if location is None:
-        return jsonify(error='Invalid JSON'), 400
-    latitude, longitude = location.get('latitude'), location.get('longitude')
-    response = jsonify(success=True)
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    print("check", latitude, longitude)
-    return latitude, longitude
-"""
+class UserMessage(BaseModel):
+    text: str
+
+messages = []
 
 def _request(host, path, api_key, url_params=None):
     url_params = url_params or {}
-    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    url = f"{host}{path}"
     headers = {
-        'Authorization': 'Bearer %s' % api_key,
+        'Authorization': f'Bearer {api_key}',
     }
-    print(u'Querying {0} ...'.format(url))
-    response = requests.request('GET', url, headers=headers, params=url_params)
+    print(f'Querying {url} ...')
+    response = requests.get(url, headers=headers, params=url_params)
     return response.json()
 
 def get_restaurant_info(latitude, longitude):
     url_params = {
-    'term': "food",
-    'latitude': latitude,
-    'longitude': longitude,
-    'limit': YELP_SEARCH_LIMIT,
-    'radius': 25 
+        'term': "food",
+        'latitude': latitude,
+        'longitude': longitude,
+        'limit': YELP_SEARCH_LIMIT,
+        'radius': 25
     }
     json_data = _request(YELP_HOST, SEARCH_PATH, yelp_api_key, url_params)
     restaurants = json_data["businesses"]
-    data = {} # name and cuisine type
-    extra = {} # address and rating
+    data = {}  # name and cuisine type
+    extra = {}  # address and rating
     for restaurant in restaurants:
         entry = {}
         entry2 = {}
@@ -91,47 +80,49 @@ def get_restaurant_info(latitude, longitude):
         location = restaurant["location"]
         rating = restaurant["rating"]
         entry = {"name": name, "categories": category_aliases}
-        entry2 = {"name": name, "location" : location, "rating" : rating, "coordinates" : coords2}
+        entry2 = {"name": name, "location": location, "rating": rating, "coordinates": coords2}
         data[id] = entry
         extra[id] = entry2
     data_json = json.dumps(data)
     extra_json = json.dumps(extra)
     return data_json, extra_json
 
-@app.route("/messages/", methods=["POST", "GET"])
-async def message_sent():
-    if request.method == "GET":
-        pass
-    elif request.method == "POST":
-        data = request.get_json()
-        if data is None:
-            return jsonify(error="Invalid JSON"), 400
-        user_message = data.get("text")
-        messages.append({"role": "user", "content": user_message})
-        response = await _send_chat_request()
-        messages.append({"role": "assistant", "content": response})
-        unique_id = abs(hash(uuid.uuid4()))
-        message_dto = {
-            "id": unique_id,
-            "date": int(time.time() * 1000),
-            "text": response['choices'][0]['message']['content'],
-            "sender": "bot",
-        }
-        return jsonify(message_dto)
+@app.post("/")
+async def init(location: Location):
+    latitude, longitude = location.latitude, location.longitude
+    data, extra_info = get_restaurant_info(latitude, longitude)
+    data_string = json.dumps(data)
+    messages.append({"role": "user", "content": "Here is data about restaurants in JSON format. Use this data to answer my questions. " + data_string})
+    messages.append({"role": "user", "content": "Here is extra info about restaurants in JSON format. Use this data to answer my questions. " + extra_info})
+    return {"success": True}
+
+@app.post("/messages/")
+async def message_sent(data: UserMessage):
+    user_message = data.text
+    messages.append({"role": "user", "content": user_message})
+    response = await _send_chat_request()
+    messages.append({"role": "assistant", "content": response})
+    unique_id = abs(hash(uuid.uuid4()))
+    message_dto = {
+        "id": unique_id,
+        "date": int(time.time() * 1000),
+        "text": response['choices'][0]['message']['content'],
+        "sender": "bot",
+    }
+    return message_dto
 
 async def _send_chat_request():
     '''
     Helper function that handles the chat request to the OpenAI API.
     '''
     response = ""
-    openai.api_key = openai_api_key
     try:
-        response: Any = await openai.ChatCompletion.acreate(
-            model= "gpt-3.5-turbo",
-            messages= messages,
+        response: Any = await openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
         )
     except Exception as exception:
-        raise OpenAIServiceError (
+        raise OpenAIServiceError(
             f"OpenAI service failed to complete the chat: {exception}"
         ) from exception
     return response
@@ -145,4 +136,6 @@ class OpenAIServiceError(Exception):
         super().__init__(message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
